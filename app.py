e32566ce08311af0982aca784754271f0933b77b
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2023-09-26 22:04:25 +0100

    edit

--- app.py
+++ app.py
@@ -17,7 +17,22 @@ with header:
 
 with syptomps:
     st.header("What symptoms are you experiencing?")
-    st.markdown("Select the symptomps below")
+    st.markdown("Select the symptoms below")
      # Create input colums
-    sel_col, disp_col = st.columns(2)   
-    symp = sel_col.selectbox("Symptoms", options=["rash", "Vomiting", "headach"])
+    sel_col_1, disp_col_1 = st.columns(2) 
+
+    symp_1 = sel_col_1.selectbox("Symptoms", options=["rash", "Vomiting", "headache", "cough"])
+
+    sel_col_2, disp_col = st.columns(2) 
+    symp_2 = sel_col_2.selectbox("More Symptoms", options=["body_rash", "Vomiting", "headache", "cough"])
+
+    
+    #sel_col_3, disp_col_3 = st.columns(2) 
+    #symp_3 = sel_col_3.selectbox("Symptoms", options=["rash", "Vomiting", "headach", "cough"])
+
+
+
+
+    disp_col_1.write("Your symptoms are:")
+    disp_col_1.write(symp_1)
+    disp_col_1.write(symp_2)
