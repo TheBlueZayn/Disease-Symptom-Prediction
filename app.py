45f7f0abe37b5ac3f3de4280eb22966eae66ed4d
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2023-09-26 21:00:02 +0100

    Configuaration
    Make streamlit runn

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-pip install anvil-app-server
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+# Create subsections 
+header = st.container()
+with header:
+    st.title("BlueZayn's Disease Prediction Model")
