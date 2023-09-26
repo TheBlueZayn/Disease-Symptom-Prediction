a2d0c0e36bc95c3d2c758d354fc7479875ba1c81
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2023-09-26 21:20:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,17 @@ import numpy as np
 # Create subsections 
 header = st.container()
 dataset = st.container()
-syptoms = st.container()
+syptomps = st.container()
 
 
 
 with header:
     st.title("BlueZayn's Disease Prediction Model")
+
+
+with syptomps:
+    st.header("What symptoms are yo experiencing?")
+    st.markdown("Select the symptomps below")
+     # Create input colums
+    sel_col, disp_col = st.columns(2)   
+    symp = sel_col.select_slider("Symptoms", options=[])
