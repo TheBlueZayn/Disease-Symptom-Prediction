39fca234d0890e40d34521b90635aadc401627d8
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2023-10-17 16:07:47 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 st.header("Test run")
 
-options = st.multiselect("syptomps", options=["itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing"], format_func=({options}))
+options = st.multiselect("syptomps", options=["itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing"], format_func=({op}))
 
 #options = options
 
