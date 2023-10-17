644dba33729823b8776f5e2f334418ff7e662035
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2023-10-17 15:36:31 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,4 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-st.write("Test run")
+st.header("Test run")
+
+options = st.multiselect("syptomps", options=["itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing"])
+st.write(options)
