5e2d388f8e59e139c08d866fd41c8cf44335b719
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2023-10-18 11:36:58 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,14 +8,17 @@ st.header("Test run")
 options = st.multiselect("syptomps", options=["itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing"])
     #symp_1 = st.selectbox("Symptoms", options= ["itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing", "shivering", "chills", "joint_pain", "stomach_pain", "acidity", "ulcers_on_tongue", "muscle_wasting", "vomiting", "burning_micturition", "spotting_urination", "fatigue", "weight_gain", "anxiety", "cold_hands_and_feets", "mood_swings", "weight_loss", "restlessness", "lethargy", "patches_in_throat", "irregular_sugar_level", "cough", "high_fever", "sunken_eyes", "breathlessness", "sweating", "dehydration", "indigestion", "headache", "yellowish_skin", "dark_urine", "nausea", "loss_of_appetite", "pain_behind_the_eyes", "back_pain", "constipation", "abdominal_pain", "diarrhoea", "mild_fever", "yellow_urine", "yellowing_of_eyes", "acute_liver_failure", "fluid_overload", "swelling_of_stomach", "swelled_lymph_nodes"])
 #options = options
-symp = []
-symp.append(options)
-st.write(symp)
+# symp = []
+# symp.append(options)
 
-symps = list(symp[0])
-st.write(symps)
+for option in options:
+    st.write(option)
 
-st.write(type(symps))
+
+# symps = (symp[0])
+# st.write(symps)
+
+# st.write(type(symps))
 
 #st.write(len(symps))
 
@@ -28,6 +31,9 @@ st.write(type(symps))
 #st.write(str(symp[0]))
 
 df = pd.read_csv("empty_df.csv")
+
+for option in options:
+    df[option] = 1
 
 # for i in len(symps):
 #     df[i] = 1
